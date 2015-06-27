cd357499dcecd01368725e57af243e93a28e5eab
NotThatSmith <smithy_122@hotmail.com>
2015-06-27 14:46:17 +1200

    First commit

---
+++ SMSServer/SMSServer/Controllers/SMSController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Azure;
+using Twilio;
+
+namespace SMSServer.Controllers
+{
+    public class SMSController : ApiController
+    {
+
+        [HttpPost]
+        public void SendMessage(Message message)
+        {
+            string AccountSid = "ACbc5892a55d03da310a1ab2663804d3d9";
+            string AuthToken = "77f97a04db323c03086cd93a363d606a";
+            var twilio = new TwilioRestClient(AccountSid, AuthToken);
+            var numbers = CloudConfigurationManager.GetSetting("ReminderNumbers").Split(',');
+            foreach (var number in numbers)
+            {
+                var twilioMessage = twilio.SendMessage(message., number, "Do some YOMO!");
+                Console.WriteLine(twilioMessage.Sid);
+            }
+        }
+    }
+}
