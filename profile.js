fd13ad6481e34a422d1ebea79c1b1a09ee9b172f
masja205 <jaymesh.master@gmail.com>
2015-06-28 14:36:08 +1200

    Update profile.js

--- profile.js
+++ profile.js
@@ -18,12 +18,9 @@ function draw(){
 // part of profile pic
 	
 	var picBox = mainBox.append("rect")
-		.attr("x", 100).attr("y", 210).attr("rx", 10).attr("ry", 10)
-		.attr("height", 100).attr("width",100)
+		.attr("x", 120).attr("y", 225).attr("rx", 10).attr("ry", 10)
+		.attr("height", 80).attr("width",70)
 		.attr("fill", "#8FCC94");
-	var contactHistory = mainBox.append("image")
-		.attr("xlink:href", "bin/github11.png")
-		.attr("x", 100).attr("y", 210).attr("width",100).attr("height", 100);
 //box level 1		
 	var userName = mainBox.append("rect")
 		.attr("x", 220).attr("y", 210).attr("rx", 10).attr("ry", 10)
@@ -90,28 +87,76 @@ var contactHistory = mainBox.append("image")
 		.attr("x", 560).attr("y", 212).attr("width",40).attr("height", 40);
 
 
+
+	var contactHistory = mainBox.append("image")
+		.attr("xlink:href", "bin/github12.png")
+		.attr("opacity", 1).attr("x", 66).attr("y", 170).attr("width",170).attr("height", 170);
+				
+drawText("@sallyuser1990",270 , 230);
+//uname logo
+var uname = mainBox.append("image")
+		.attr("xlink:href", "bin/user77.png")
+		.attr("x", 229).attr("y", 210).attr("width",30).attr("height", 30);
+//phone
+drawText("+64221850365",270 , 255);
+var uname = mainBox.append("image")
+		.attr("xlink:href", "bin/telephone65.png")
+		.attr("x", 229).attr("y", 235).attr("width",30).attr("height", 30);
+//skype
+drawText("Sal.call1990",270 , 279);
+var uname = mainBox.append("image")
+		.attr("xlink:href", "bin/skype9.png")
+		.attr("x", 229).attr("y", 260).attr("width",30).attr("height", 30);
 		
-drawText("@Username1990",230 , 230);
-drawText("Friday Skills Group",230 , 290);
-drawText("Mob: +64221850365",230 , 260);
+//therapy group
+drawText("Friday Skills Group",270 , 303);
+var uname = mainBox.append("image")
+		.attr("xlink:href", "bin/group41.png")
+		.attr("x", 231).attr("y", 285).attr("width",25).attr("height", 25)
+
+//helpful
+var uname = mainBox.append("image")
+		.attr("xlink:href", "bin/thumbsups.png")
+		.attr("x", 100).attr("y", 330).attr("width",25).attr("height", 25)
+drawHeader("Helpful:",130 , 350);
+drawText("- Spend time with Pet (Nemo)",130 , 370);
+//unhelpful
+
+var uname = mainBox.append("image")
+		.attr("xlink:href", "bin/thumbsdown.png")
+		.attr("x", 340).attr("y", 335).attr("width",25).attr("height", 25)
+		
+drawHeader("Unhelpful:",370 , 350);
+drawText("- Txt Boyfriend",370 , 370);
+drawText("- Shopping",370 , 390);
 
 
-drawText("Helpful:",110 , 350);
-drawText("- Spend time with Pet (Nemo)",110 , 370);
+//risk
+var uname = mainBox.append("image")
+		.attr("xlink:href", "bin/medkit.png")
+		.attr("x", 100).attr("y", 520).attr("width",25).attr("height", 25)
+drawHeader("Risk Behaviours:",130 , 537);
+drawText("- Self Harm (Cutting)",130 , 557);
+drawText("- Substance Abuse (Alcohol)",130 , 577);
+//support people
+drawHeader("Support People", 350 , 537);
+var uname = mainBox.append("image")
+		.attr("xlink:href", "bin/heart74.png")
+		.attr("x", 320).attr("y", 520).attr("width",25).attr("height", 25)
 
-drawText("Unhelpful:",110 , 420);
-drawText("- Txt Boyfriend",110 , 440);
-drawText("- Shopping",110 , 460);
-drawText("Risk Behaviours:",110 , 537);
-drawText("- Self Harm (Cutting)",110 , 557);
-drawText("- Substance Abuse (Alcohol)",110 , 577);
-drawText("Support People", 350 , 537);
 drawText("Best Friend: ", 350 , 555);
-drawText("Maggie McDonald +642219603645", 350 , 570);
+drawText("Maggie McDonald", 350 , 575);
+drawText("+642219603645", 350 , 595);
+
+
 function drawText(text,x,y){
 text = mainBox.append("text")
-.attr("x", x).attr("y", y).text(text).attr("font-size", 15).style("font-weight", 5).attr("class","text");
-}		
+.attr("x", x).attr("y", y).text(text).attr("font-size", 1).style("font-weight", 5).attr("class","text");
+}
+function drawHeader(text,x,y){
+text = mainBox.append("text")
+.attr("x", x).attr("y", y).text(text).attr("font-size", 20).style("font-weight", 30).attr("class","header");
+}			
 function mouseoverChange(d){
 d.attr("fill", colour2).attr("opacity", .3);
 }
