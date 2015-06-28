6dab483896d06dc228cdab9f5d6bbef671a2e1bd
return-string <v.mckay@gmx.com>
2015-06-28 14:01:35 +1200

    Update call_1.js

--- call_1.js
+++ call_1.js
@@ -3,13 +3,16 @@ call();
 function call(){
 	width = 500, colour1="white", colour2="steelblue";
 	input_height = 50;
+	
+	selectedPatient = "Sally";
+	var numPatients = 4; // 1. get the patients' icons from the file
+	
 	var name, message, time;
 	var body = d3.select("body");
 	/*The main workspace for the message box*/
 	var svg = body.append("svg")
 		.attr("height", 1000)
 		.attr("width", 1500);
-		console.log("getting here");
 	var mainBox = svg.append("svg")
 		.attr("width", 1500).attr("height", 1000);
 		
@@ -19,21 +22,67 @@ function call(){
 		.attr("fill", "lightgray").attr("opacity", .5);
 	
 	var banner = mainBox.append("image")
-	.attr("x", 75).attr("y", 110).attr("height", 85).attr("width", 600)
-	.attr("xlink:href", "bin/banner.png");
+		.attr("x", 75).attr("y", 110).attr("height", 85).attr("width", 600)
+		.attr("xlink:href", "bin/banner.png");
 	var line = mainBox.append("line")
-	.attr("x1", 100).attr("x2",600).attr("y1", 200).attr("y2", 200).style("stroke","gray");
+		.attr("x1", 100).attr("x2",600).attr("y1", 200).attr("y2", 200).style("stroke","gray");
 		
-	var issueBox = mainBox.append("rect")
+	// draw box containing 'Find patient...' text
+	var nameBox = mainBox.append("rect")
 		.attr("x", 150).attr("y", 370).attr("rx", 5).attr("ry", 5)
 		.attr("height", 50).attr("width", width-100)
 		.attr("fill", "white").attr("stroke", "black").attr("stroke-width", 0.2)
-		.on("mouseover", function(d) {mouseoverChange(d3.select(this))})
-		.on("mouseout", function(d) {mouseoutChange(d3.select(this))});
+		.on("mouseover", function(d) { mouseoverChange(d3.select(this)) })
+		.on("mouseout", function(d) { mouseoutChange(d3.select(this)) })
+		.on("click", function(d) { 
+						d3.selectAll(".dropdown-toggle").style({'display':'block'}); } );
 	
-		/* Will show a dropdown box to select the name of required patient from JSON */
 	var nameText = mainBox.append("text")
-		.attr("x", 175).attr("y", 405).text("Find patient...").attr("font-size", 30);
+		.attr("x", 175).attr("y", 405).text("Find patient...").attr("font-size", 30)
+		.attr("class","large-title")
+		.attr("id","name");
+	var patientText = mainBox.append("text")
+		.attr("x", 175).attr("y", 405).text("").attr("font-size", 30)
+		.attr("class","large-title");
+	var patientImg = mainBox.append("image")
+		.attr("x", 150).attr("y", 405).attr("width",35).attr("height", 35)
+		.attr("xlink:href", "")
+		.attr("class","large-text");
+		
+	// DROPDOWN BOX BEGINS
+	
+	// 2. draw the dropdown menu
+	var dropdown = mainBox.append("rect");
+	dropdown
+		.style({'display':'none'}) // dropdown should start out hidden
+		.attr("class","dropdown-toggle")
+		.attr("x", 150).attr("y", 370).attr("rx", 5).attr("ry", 5)
+		.attr("height", (60*numPatients) + 10*((numPatients>0)?(numPatients-1):0)).attr("width", width-100)
+		.attr("fill", "white").attr("stroke", "black").attr("stroke-width", 0.2);
+	// 3. fill in the contents of the menu
+	for(i = 0; i < 1; i++) { //>
+		var patientName = "sallyuser1990"; // gotta populate this somehow
+		mainBox.append("rect")
+			.attr("display","none")
+			.attr("class","dropdown-toggle")
+			.attr("id",patientName)
+			.attr("x", 151)
+			.attr("y", 370+(50*i + (1*( i + 1) + 1)))
+			.attr("rx", 5).attr("ry", 5)
+			.attr("height",50).attr("width",width-102)
+    		.attr("fill", "white").attr("stroke", "black").attr("stroke-width", 0.2)
+    		.on("mouseover", function(d) { mouseoverChange(d3.select(this)) })
+    		.on("mouseout", function(d) { mouseoutChange(d3.select(this)) })
+			.on("click",function(d) { 
+			  console.log("Hi, Sally!"); 
+			  d3.selectAll(".dropdown-toggle").style({'display':'none'});
+			  selectedPatient = d3.select('#sallyuser1990').attr("id");
+			  d3.select("#name").text("sallyuser1990"); });
+		mainBox.append("text")
+			.attr("class","text dropdown-toggle")
+			.style({'display':'none'})
+			.attr("x", 170).attr("y", 405 + (50*i + (1*(i+1) + 1)) ).text(patientName).attr("font-size", 25);
+	}
 		
 	var backButton = mainBox.append("image")
 		.attr("x", 55).attr("y", 110).attr("width",35).attr("height", 35)
@@ -49,11 +98,11 @@ function call(){
 		.on("mouseout", function(d) {mouseoutChange(d3.select(this))})
 		.on("click", function(d) { location.href = "loadacall_2.html"});
 
-function mouseoverChange(d){
-d.attr("fill", colour2).attr("opacity", .3);
-}
-
-function mouseoutChange(d){
-d.attr("fill", colour1).attr("opacity", 1);
-}	
+    function mouseoverChange(d){
+   		d.attr("fill", colour2).attr("opacity", .3);
+    }
+    
+    function mouseoutChange(d){
+    	d.attr("fill", colour1).attr("opacity", 1);
+    }
 }
